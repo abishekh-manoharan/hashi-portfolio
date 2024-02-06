5b974b04be05c2c62fdd27cc7474a91d4d62a8db
abi-manoharan97 <abishekh.manoharan@gmail.com>
2024-02-06 12:23:04 -0500

    mobile homepage styling

--- fe-hashveenah-portfolio/src/components/Home.tsx
+++ fe-hashveenah-portfolio/src/components/Home.tsx
@@ -2,11 +2,20 @@ import { Link } from "react-router-dom";
 
 function Home() {
     return (
-        <div>
-            Hashveenah Manoharan
-            <Link to='about'>About</Link>
-            <Link to='projects'>Projects</Link>
-            <Link to='contact'>Contact</Link>
+        <div className="home">
+            <video id="home-background-video" autoPlay loop muted>
+                <source src="./videos/sunlight.mp4" type="video/mp4" />
+            </video>
+            <div className="home-content">
+
+                <div className="name">Hashveenah Manoharan</div>
+                <div className="title">Arborist, Artist</div>
+                <div className="home-nav">
+                    <div className="home-nav-link"><Link to='about'>ABOUT</Link></div>
+                    <div className="home-nav-link"><Link to='projects'>PROJECTS</Link></div>
+                    <div className="home-nav-link"><Link to='contact'>CONTACT</Link></div>
+                </div>
+            </div>
         </div>
     );
 }
