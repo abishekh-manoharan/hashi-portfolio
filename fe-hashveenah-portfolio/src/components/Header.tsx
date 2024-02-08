b2c2ab873b97ec0204e4316ad4bded7ff6ab306a
abi-manoharan97 <abishekh.manoharan@gmail.com>
2024-02-08 10:07:53 -0500

    close header menu if user clicks outside of it

--- fe-hashveenah-portfolio/src/components/Header.tsx
+++ fe-hashveenah-portfolio/src/components/Header.tsx
@@ -1,18 +1,24 @@
 import { Outlet } from "react-router-dom";
+import HeaderMenu from "./HeaderMenu";
+import { useState } from "react";
+// import { useEffect, useState } from "react";
 
 interface headerProps {
-    location :string;
+    location: string;
 }
 
-function Header( props: headerProps ) {
+function Header(props: headerProps) {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <>
+            <HeaderMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+            <img className="header-menu-logo" src="./images/menu.svg" onClick={() => setMenuOpen(!menuOpen)}/>
             <div className="header">
                 <div className="header-name">Hashveenah Manoharan</div>
-                <div className="page-title">{props.location}</div>
-                <div className="header-menu"><div>Menu</div></div>
+                <div className="header-page-title">{props.location}</div>
             </div>
-            <Outlet/>
+            <Outlet />
         </>
     );
 }
---
+++ fe-hashveenah-portfolio/src/components/HeaderMenu.tsx
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import { NavLink } from "react-router-dom";
+
+interface headerMenuProps {
+    menuOpen: boolean;
+    setMenuOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+function HeaderMenu(props: headerMenuProps) {
+    
+    useEffect(() => { // ensures that if the user clicks outside of the menu, that the menu closes
+        const menu = document.querySelector('.header-menu');
+        const menuLogo = document.querySelector('.header-menu-logo');
+        
+        const clickHandler = (e: MouseEvent) => {
+            e.stopPropagation();
+            if(e.target instanceof Node) {
+                if(!menu!.contains(e.target) && !menuLogo!.contains(e.target)) {
+                    props.setMenuOpen(false);
+                }
+            }
+        }
+
+        window.addEventListener('click', clickHandler);
+
+        return () => {
+            window.removeEventListener('click', clickHandler);
+        }
+    }, []);
+
+
+    return (
+        <div className={`header-menu ${props.menuOpen ? 'animation-open-menu-header' : ''}`}>
+            <NavLink className="header-menu-NavLink" onClick={() => props.setMenuOpen(!props.menuOpen)}to='/'>HOME</NavLink>
+            <NavLink className="header-menu-NavLink" onClick={() => props.setMenuOpen(!props.menuOpen)}to='about'>ABOUT</NavLink>
+            <NavLink className="header-menu-NavLink" onClick={() => props.setMenuOpen(!props.menuOpen)}to='projects'>PROJECTS</NavLink>
+            <NavLink className="header-menu-NavLink" onClick={() => props.setMenuOpen(!props.menuOpen)}to='contact'>CONTACT</NavLink>
+        </div>
+    );
+}
+
+export default HeaderMenu;
