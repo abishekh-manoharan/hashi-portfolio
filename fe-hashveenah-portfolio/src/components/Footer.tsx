f7952c13521ad3625d6efea36ba08fdd99bc1227
abi-manoharan97 <abishekh.manoharan@gmail.com>
2024-02-28 11:39:59 -0500

    footer

---
+++ fe-hashveenah-portfolio/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <div className="footer">
+            <a target="_" href="https://www.instagram.com/hashveenah/?hl=en"><img className="footer-instagram footer-logo" src="/images/instagram-logo.svg"/></a>
+            <a target="_" href="https://ca.linkedin.com/in/hashveenah-manoharan-68715a14b"><img className="footer-linkedin footer-logo" src="/images/linkedin-logo.svg" /></a>
+        </div>
+    );
+}
+
+export default Footer;
--- fe-hashveenah-portfolio/src/components/Header.tsx
+++ fe-hashveenah-portfolio/src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { NavLink, Outlet } from "react-router-dom";
 import HeaderMenu from "./HeaderMenu";
 import { useState } from "react";
+import Footer from "./Footer";
 // import { useEffect, useState } from "react";
 
 interface headerProps {
@@ -13,28 +14,28 @@ function Header(props: headerProps) {
     return (
         <div className="headerAndOutlet">
             <div className="header-container">
-                    <HeaderMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-                    <div className="header-content">
-                        <img className="header-menu-logo" src="./images/menu.svg" onClick={() => { console.log('clock'); setMenuOpen(!menuOpen) }} />
-                        <div className="header-name">Hashveenah Manoharan</div>
-                        <div className="header-page-title">{props.location}</div>
-                        <div className="header-nav">
-                            <NavLink className="header-NavLink" to='/'>HOME</NavLink>
-                            <NavLink className="header-NavLink" to='about'>ABOUT</NavLink>
-                            <NavLink className="header-NavLink" to='projects'>SELECTED WORKS</NavLink>
-                            <NavLink className="header-NavLink" to='contact'>CONTACT</NavLink>
-                        </div>
-                        {/* <div className="header-footer">
-                            Copyright xyz
-                        </div> */}
+                <HeaderMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+                <div className="header-content">
+                    <img className="header-menu-logo" src="./images/menu.svg" onClick={() => { console.log('clock'); setMenuOpen(!menuOpen) }} />
+                    <div className="header-name">Hashveenah Manoharan</div>
+                    <div className="header-page-title">{props.location}</div>
+                    <div className="header-nav">
+                        <NavLink className="header-NavLink" to='/'>HOME</NavLink>
+                        <NavLink className="header-NavLink" to='about'>ABOUT</NavLink>
+                        <NavLink className="header-NavLink" to='projects'>SELECTED WORKS</NavLink>
+                        <NavLink className="header-NavLink" to='contact'>CONTACT</NavLink>
+                    </div>
+                    <div className="footer-desktop">
+                        <Footer />
+                    </div>
                 </div>
             </div>
             <div className="outlet">
                 <Outlet />
             </div>
-            {/* <div className="footer header-footer">
-                FOOTER
-            </div> */}
+            <div className="footer-mobile">
+                <Footer />
+            </div>
         </div>);
 }
 export default Header;
