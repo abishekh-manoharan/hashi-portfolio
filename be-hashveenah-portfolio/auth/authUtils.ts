ddbca806f4cf605bdd03c54dd383c9ff917ef88b
abi-manoharan97 <abishekh.manoharan@gmail.com>
2024-05-08 13:02:20 -0400

    integrated async scripts into main script to allow for synchronosity

--- be-hashveenah-portfolio/auth/authUtils.ts
+++ be-hashveenah-portfolio/auth/authUtils.ts
@@ -1,5 +1,6 @@
 import User from '../models/user';
 import crypto from 'node:crypto';
+import { doneType } from '../types';
 // import { ExistingUser, newUser } from '../types';
 
 // hashes the given password and compares is to the stored hashed value. returns true if matching, false otherwise.
@@ -10,27 +11,51 @@ const verifyPassword = (password: string, hash: string, salt: string): boolean =
 
 // generates a salt and hashed version of the password, then stores into the db
 const generateUser = (username: string, password: string) => {
-        // generate salt 
-        const salt = crypto.randomBytes(32).toString('hex');
-    
-        // generate hash
-        const genHash = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex');
-    
-        // save username, hash, salt
-        const newUser = new User({
-            username: username,
-            hash: genHash,
-            salt: salt
-        });
-    
-        newUser.save().then(user => {
-            if(!user) {
-                throw new Error('unable to save user')
-            }
+    // generate salt 
+    const salt = crypto.randomBytes(32).toString('hex');
+
+    // generate hash
+    const genHash = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex');
+
+    // save username, hash, salt
+    const newUser = new User({
+        username: username,
+        hash: genHash,
+        salt: salt
+    });
+
+    newUser.save().then(user => {
+        if (!user) {
+            throw new Error('unable to save user')
+        }
+    })
+}
+
+const customFields = {
+    usernameField: 'username',
+    passwordField: 'password'
+}
+
+const validationFunction = (username: string, password: string, done: doneType) => {
+    try {
+        User.findOne({ username: username }).then((user) => {
+            if (!user) { return done(null, false) } // case when user cannot be found
+
+            const passwordVerification = verifyPassword(password, user.hash!, user.salt!) // returns true if password matches hashed value, false otherwise
+
+            // if verified, return done with user. otherwise done with false
+            // we call user.toJSON so that the id field of the user can be populated as specified in the schema options set in ../models/user
+            passwordVerification ? done(null, user.toJSON()) : done(null, false);
         })
+    }
+    catch (err) {
+        done(err)
+    }
 }
 
 export {
     verifyPassword,
-    generateUser
+    generateUser,
+    customFields,
+    validationFunction
 };
--- be-hashveenah-portfolio/index.ts
+++ be-hashveenah-portfolio/index.ts
@@ -4,16 +4,37 @@ import mongoStore from 'connect-mongo';
 import passport from 'passport';
 import EntryRouter from './routes/entry';
 import AuthRouter from './routes/auth';
+import UserRouter from './routes/user';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import passportLocal from 'passport-local';
+import { validationFunction, customFields } from './auth/authUtils';
+import User from './models/user';
+import { userType } from './types';
 
-import './utils/config'
-// import { session } from 'passport';
+
 const app = express();
 
+// initislize env variables
+dotenv.config();
+
+// connecting to mongodb
+if (typeof (process.env.MONGO_URL) === 'string') {
+    mongoose.connect(process.env.MONGO_URL)
+        .then(() => { console.log('connected to mongodb') });
+}
+console.log('process.env.FE_URL');
+console.log(process.env.FE_URL);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-
+app.use(
+    cors({
+      origin: 'http://localhost:5173',
+      methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
+      credentials: true,
+    })
+  );
 //setting up session
 const store = mongoStore.create({
     mongoUrl: process.env.MONGO_URL,
@@ -22,28 +43,49 @@ const store = mongoStore.create({
 
 const session = sessions({
     secret: process.env.SECRET_KEY!,
-    resave: true,
-    saveUninitialized: true,
+    resave: false,
+    saveUninitialized: false,
     store: store,
-    cookie: {
+    cookie: {      
+        sameSite: false,
+        httpOnly: true,
         maxAge: 1000 * 60 * 5 // 5 min
     }
 })
 
 app.use(session);
 
-// setting up passport
-import './auth/passport';
 
-app.use(passport.session());
+
+// setting up passport
+const LocalStrategy = passportLocal.Strategy;
+const localStrategy = new LocalStrategy(customFields, validationFunction);
+passport.use(localStrategy);
+
+passport.serializeUser((user: userType, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((user_id, done) => {
+    User.findById(user_id)
+        .then((user) => {
+            done(null, user)
+        })
+        .catch((err) => done(err))
+});
+
 app.use(passport.initialize());
+app.use(passport.session());
 
 // routes
 app.use('/entry', EntryRouter);
 app.use('/auth', AuthRouter);
-app.get('/', (req,res) => {
+app.use('/user', UserRouter);
+app.get('/', (req, res) => {
     res.send('hello world!')
 })
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+
+
