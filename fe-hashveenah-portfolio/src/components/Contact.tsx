8f276ae53cb4a93a4adc530e15f59345a134a075
abi-manoharan97 <abishekh.manoharan@gmail.com>
2024-05-27 14:21:36 -0400

    validity check, object-fit for contact page

--- fe-hashveenah-portfolio/src/components/Contact.tsx
+++ fe-hashveenah-portfolio/src/components/Contact.tsx
@@ -17,43 +17,51 @@ function Contact(props: contactProps) {
         e.preventDefault();
         e.stopPropagation();
         console.log('button clicked')
-    }
-    return (
-        <div className="contact">
-            {/* <label className="form-label" htmlFor="username">E-Mail</label> */}
-            <div className="contact-info">
-                {/* <img src="/images/moose.jpg" className="contact-info-background"/> */}
-                <img src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="contact-info-background" />
-                <div className="contact-info-entry">
-                    <img className="contact-info-entry-logo" src="/images/mail-logo.svg" />
-                    <div className="contact-info-entry-header">
-                        Email
-                    </div>
-                    <div className="contact-info-entry-value">
-                        hashveenah@gmail.com
-                    </div>
-                </div>
-                <div className="contact-info-entry">
-                    <img className="contact-info-entry-logo" src="/images/map-pin-logo.svg" />
-                    <div className="contact-info-entry-header">
-                        Based In
-                    </div>
-                    <div className="contact-info-entry-value">
-                        Toronto, Ontario
-                    </div>
-                </div>
-            </div>
-            <div className="contact-header">
-                <p className="contact-header-text">
-                    Get in touch
-                </p>
-            </div>
-            <input className="form-input" onChange={(e) => { setName(e.target.value) }} type='text' name='name' placeholder='Full Name' required /> <br />
-            <input className="form-input" onChange={(e) => { setEmail(e.target.value) }} type='email' name='email' placeholder='E-Mail' required /> <br />
-            <textarea className="form-input" onChange={(e) => { setMessage(e.target.value) }} name='message' rows={5} placeholder='Message.....' required /><br />
-            <button type="submit" onClick={submitContactMessageHandler} className="login-form-submit-btn config-add-new-collection-btn" >Contact</button>
-        </div>
-    );
-}
 
-export default Contact;
+        const form = document.querySelector(".contact-form") as HTMLSelectElement;
+        if (!form.checkValidity()) {
+            form.reportValidity(); // reports on validity issues
+            return; // breaks out of method if validity issues exist
+        }
+    }
+        return (
+            <div className="contact">
+                {/* <label className="form-label" htmlFor="username">E-Mail</label> */}
+                <div className="contact-info">
+                    {/* <img src="/images/moose.jpg" className="contact-info-background"/> */}
+                    <img src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="contact-info-background" />
+                    <div className="contact-info-entry">
+                        <img className="contact-info-entry-logo" src="/images/mail-logo.svg" />
+                        <div className="contact-info-entry-header">
+                            Email
+                        </div>
+                        <div className="contact-info-entry-value">
+                            hashveenah@gmail.com
+                        </div>
+                    </div>
+                    <div className="contact-info-entry">
+                        <img className="contact-info-entry-logo" src="/images/map-pin-logo.svg" />
+                        <div className="contact-info-entry-header">
+                            Based In
+                        </div>
+                        <div className="contact-info-entry-value">
+                            Toronto, Ontario
+                        </div>
+                    </div>
+                </div>
+                <div className="contact-header">
+                    <p className="contact-header-text">
+                        Get in touch
+                    </p>
+                </div>
+                <form className="contact-form">
+                    <input className="form-input" onChange={(e) => { setName(e.target.value) }} type='text' name='name' placeholder='Full Name' required /> <br />
+                    <input className="form-input" onChange={(e) => { setEmail(e.target.value) }} type='email' name='email' placeholder='E-Mail' required /> <br />
+                    <textarea className="form-input" onChange={(e) => { setMessage(e.target.value) }} name='message' rows={5} placeholder='Message.....' required /><br />
+                    <button type="submit" onClick={submitContactMessageHandler} className="login-form-submit-btn config-add-new-collection-btn" >Contact</button>
+                </form>
+            </div>
+        );
+    }
+
+    export default Contact;
