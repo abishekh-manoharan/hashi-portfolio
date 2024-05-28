edcff2c655cc77093c51b161352f38ed0cc5b373
abi-manoharan97 <abishekh.manoharan@gmail.com>
2024-05-28 13:46:57 -0400

    desktop contact page UI

--- fe-hashveenah-portfolio/src/components/Contact.tsx
+++ fe-hashveenah-portfolio/src/components/Contact.tsx
@@ -24,44 +24,51 @@ function Contact(props: contactProps) {
             return; // breaks out of method if validity issues exist
         }
     }
-        return (
-            <div className="contact">
-                {/* <label className="form-label" htmlFor="username">E-Mail</label> */}
-                <div className="contact-info">
-                    {/* <img src="/images/moose.jpg" className="contact-info-background"/> */}
-                    <img src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="contact-info-background" />
-                    <div className="contact-info-entry">
-                        <img className="contact-info-entry-logo" src="/images/mail-logo.svg" />
-                        <div className="contact-info-entry-header">
-                            Email
-                        </div>
-                        <div className="contact-info-entry-value">
-                            hashveenah@gmail.com
-                        </div>
+    return (
+        <div className="contact">
+            {/* <label className="form-label" htmlFor="username">E-Mail</label> */}
+            <img className="contact-img-desktop" src="/images/contact-desktop.jpeg" />
+            <div className="contact-info">
+                <hr/>
+                {/* <img src="/images/moose.jpg" className="contact-info-background"/> */}
+                <img className="contact-info-img-mobile" src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+
+                <div className="contact-info-entry">
+                    <img className="contact-info-entry-logo" src="/images/mail-logo.svg" />
+                    <img className="contact-info-entry-logo-dark" src="/images/mail-logo-dark.svg" />
+                    <div className="contact-info-entry-header">
+                        Email
                     </div>
-                    <div className="contact-info-entry">
-                        <img className="contact-info-entry-logo" src="/images/map-pin-logo.svg" />
-                        <div className="contact-info-entry-header">
-                            Based In
-                        </div>
-                        <div className="contact-info-entry-value">
-                            Toronto, Ontario
-                        </div>
+                    <div className="contact-info-entry-value">
+                        hashveenah@gmail.com
                     </div>
                 </div>
+                <div className="contact-info-entry">
+                    <img className="contact-info-entry-logo" src="/images/map-pin-logo.svg" />
+                    <img className="contact-info-entry-logo-dark" src="/images/map-pin-logo-dark.svg" />
+                    <div className="contact-info-entry-header">
+                        Based In
+                    </div>
+                    <div className="contact-info-entry-value">
+                        Toronto, Ontario
+                    </div>
+                </div>
+                
+            </div>
+            <img src="/public/images/vine.png" className="vineArt" alt="" />
+            <form className="contact-form">
                 <div className="contact-header">
                     <p className="contact-header-text">
                         Get in touch
                     </p>
                 </div>
-                <form className="contact-form">
-                    <input className="form-input" onChange={(e) => { setName(e.target.value) }} type='text' name='name' placeholder='Full Name' required /> <br />
-                    <input className="form-input" onChange={(e) => { setEmail(e.target.value) }} type='email' name='email' placeholder='E-Mail' required /> <br />
-                    <textarea className="form-input" onChange={(e) => { setMessage(e.target.value) }} name='message' rows={5} placeholder='Message.....' required /><br />
-                    <button type="submit" onClick={submitContactMessageHandler} className="login-form-submit-btn config-add-new-collection-btn" >Contact</button>
-                </form>
-            </div>
-        );
-    }
+                <input className="form-input" onChange={(e) => { setName(e.target.value) }} type='text' name='name' placeholder='Full Name' required /> <br />
+                <input className="form-input" onChange={(e) => { setEmail(e.target.value) }} type='email' name='email' placeholder='E-Mail' required /> <br />
+                <textarea className="form-input" onChange={(e) => { setMessage(e.target.value) }} name='message' rows={10} placeholder='Message.....' required /><br />
+                <button type="submit" onClick={submitContactMessageHandler} className="login-form-submit-btn config-add-new-collection-btn" >Send</button>
+            </form>
+        </div>
+    );
+}
 
-    export default Contact;
+export default Contact;
