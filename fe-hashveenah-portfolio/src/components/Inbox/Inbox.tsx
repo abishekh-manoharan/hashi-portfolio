c927047844447a7b919d390a698bbea854ab16b5
abi-manoharan97 <abishekh.manoharan@gmail.com>
2024-05-30 14:05:07 -0400

    delete all messages functionality

--- fe-hashveenah-portfolio/src/components/Inbox/Inbox.tsx
+++ fe-hashveenah-portfolio/src/components/Inbox/Inbox.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getAllMessages } from "../../services/inbox";
+import { deleteAllMessages, getAllMessages } from "../../services/inbox";
 import { Message } from "../../types";
 import MessageC from "./Message";
 
@@ -12,23 +12,36 @@ function Inbox(props: InboxProps) {
 
     useEffect(() => {
         props.setLocation('inbox')
-        
+
         getAllMessages()
             .then(res => setMessages(res));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+    const deleteAllClickHandler = (e: React.SyntheticEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        deleteAllMessages()
+            .then(res => {
+                console.log(res)
+                if(res.acknowledged) {
+                    setMessages([]);
+                }
+            });
+    }
 
     return (
         <div className="inbox">
             <h1>Inbox</h1>
+            <button onClick={deleteAllClickHandler} className="config-collections-image-delete-btn inbox-delete-all">Delete All</button>
             {/* // inbox header: message - name - email */}
             {/* // inbox header: desktop only*/}
 
             {/* // message component  */}
             {messages.map(msg => {
                 console.log(msg)
-                return <MessageC key={msg._id} id={msg._id} message={msg.message} name={msg.name} email={msg.email} setMessages={setMessages}/>
+                return <MessageC key={msg._id} id={msg._id} message={msg.message} name={msg.name} email={msg.email} setMessages={setMessages} />
             })}
         </div>
     );
