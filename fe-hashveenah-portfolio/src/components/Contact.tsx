04c946b58431ef0ac6caf8c72cf7e3ffbb265477
abi-manoharan97 <abishekh.manoharan@gmail.com>
2024-05-30 13:40:52 -0400

    send message functionality with error and success messages

--- fe-hashveenah-portfolio/src/components/Contact.tsx
+++ fe-hashveenah-portfolio/src/components/Contact.tsx
@@ -1,4 +1,5 @@
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { sendMessage } from "../services/inbox";
 
 interface contactProps {
     setLocation: Dispatch<SetStateAction<string>>;
@@ -23,13 +24,42 @@ function Contact(props: contactProps) {
             form.reportValidity(); // reports on validity issues
             return; // breaks out of method if validity issues exist
         }
+
+        const messageToSend = { name, message, email };
+
+        sendMessage(messageToSend)
+            .then(res => {
+                if (res) {
+                    setName('');
+                    setEmail('');
+                    setMessage('');
+
+                    // display error message if message doesn't send
+                    const errMsg = document.querySelector('.contact-form .success-message');
+                    errMsg?.classList.toggle("display-block");
+
+                    setTimeout(() => {
+                        errMsg?.classList.toggle("display-block");
+                    }, 2000)
+
+                } else {
+                    // display error message if message doesn't send
+                    const errMsg = document.querySelector('.contact-form .error-message');
+                    errMsg?.classList.toggle("display-block");
+
+                    setTimeout(() => {
+                        errMsg?.classList.toggle("display-block");
+                    }, 2000)
+
+                }
+            });
     }
     return (
         <div className="contact">
             {/* <label className="form-label" htmlFor="username">E-Mail</label> */}
             <img className="contact-img-desktop" src="/images/contact-desktop.jpeg" />
             <div className="contact-info">
-                <hr/>
+                <hr />
                 {/* <img src="/images/moose.jpg" className="contact-info-background"/> */}
                 <img className="contact-info-img-mobile" src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
 
@@ -53,7 +83,7 @@ function Contact(props: contactProps) {
                         Toronto, Ontario
                     </div>
                 </div>
-                
+
             </div>
             <img src="/public/images/vine.png" className="vineArt" alt="" />
             <form className="contact-form">
@@ -62,10 +92,12 @@ function Contact(props: contactProps) {
                         Get in touch
                     </p>
                 </div>
-                <input className="form-input" onChange={(e) => { setName(e.target.value) }} type='text' name='name' placeholder='Full Name' required /> <br />
-                <input className="form-input" onChange={(e) => { setEmail(e.target.value) }} type='email' name='email' placeholder='E-Mail' required /> <br />
-                <textarea className="form-input" onChange={(e) => { setMessage(e.target.value) }} name='message' rows={10} placeholder='Message.....' required /><br />
+                <input className="form-input" value={name} onChange={(e) => { setName(e.target.value) }} type='text' name='name' placeholder='Full Name' required /> <br />
+                <input className="form-input" value={email} onChange={(e) => { setEmail(e.target.value) }} type='email' name='email' placeholder='E-Mail' required /> <br />
+                <textarea className="form-input" value={message} onChange={(e) => { setMessage(e.target.value) }} name='message' rows={10} placeholder='Message.....' required /><br />
                 <button type="submit" onClick={submitContactMessageHandler} className="login-form-submit-btn config-add-new-collection-btn" >Send</button>
+                <p className="error-message">Error sending message. Please try again later.</p>
+                <p className="success-message">Message Sent</p>
             </form>
         </div>
     );
