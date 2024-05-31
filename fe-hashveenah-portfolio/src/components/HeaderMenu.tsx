459a954dbdf57753c7b72be8c452270f1e0d69e2
abi-manoharan97 <abishekh.manoharan@gmail.com>
2024-05-31 11:30:23 -0400

    updated mobile navbar to account for authentication, config page, inbox

--- fe-hashveenah-portfolio/src/components/HeaderMenu.tsx
+++ fe-hashveenah-portfolio/src/components/HeaderMenu.tsx
@@ -1,7 +1,9 @@
 // Component for the mobile menu that opens up with an animation on click of the menu button
 
-import { useEffect } from "react";
-import { NavLink } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { AuthContext } from "../utils/context";
+import authServices from "../services/auth";
 
 interface headerMenuProps {
     menuOpen: boolean;
@@ -11,6 +13,22 @@ interface headerMenuProps {
 function HeaderMenu(props: headerMenuProps) {
 
     const { setMenuOpen } = props;
+    const auth = useContext(AuthContext);
+    const nav = useNavigate();
+
+    const logoutHandler = (e: React.SyntheticEvent) => {
+        e.stopPropagation();
+        props.setMenuOpen(!props.menuOpen)
+        authServices.logout().then(res => {
+            if (res === "success") {
+                auth.setAuth!(false);
+                nav('/');
+            }
+            else {
+                window.alert('error with logout: ' + res);
+            }
+        })
+    }
 
     useEffect(() => { // ensures that if the user clicks outside of the menu, that the menu closes
         const menu = document.querySelector('.header-menu');
@@ -32,19 +50,23 @@ function HeaderMenu(props: headerMenuProps) {
         }
     }, [setMenuOpen]);
 
-    const navLinkActiveStyle = { 
+
+    const navLinkActiveStyle = {
         color: "#B47B84",
         // borderLeft: "solid",
         // paddingLeft: "5px"
     };
- 
+
     return (
         <div className={`header-menu ${props.menuOpen ? 'animation-open-menu-header' : ''}`}>
             {/* <NavLink className="header-menu-NavLink" onClick={() => props.setMenuOpen(!props.menuOpen)} to='/'>HOME</NavLink> */}
-            <NavLink className="header-menu-NavLink" style={({isActive})=>isActive ? navLinkActiveStyle : {}} onClick={() => props.setMenuOpen(!props.menuOpen)} to='/'>Home</NavLink>
-            <NavLink className="header-menu-NavLink" onClick={() => props.setMenuOpen(!props.menuOpen)} style={({isActive})=>isActive ? navLinkActiveStyle : {}} to='projects'>Selected Works</NavLink>
-            <NavLink className="header-menu-NavLink" onClick={() => props.setMenuOpen(!props.menuOpen)} style={({isActive})=>isActive ? navLinkActiveStyle : {}} to='contact'>Contact</NavLink>
-            <NavLink className="header-menu-NavLink" onClick={() => props.setMenuOpen(!props.menuOpen)} style={({isActive})=>isActive ? navLinkActiveStyle : {}} to='login'>Login</NavLink>
+            <NavLink className="header-menu-NavLink" style={({ isActive }) => isActive ? navLinkActiveStyle : {}} onClick={() => props.setMenuOpen(!props.menuOpen)} to='/'>Home</NavLink>
+            <NavLink className="header-menu-NavLink" onClick={() => props.setMenuOpen(!props.menuOpen)} style={({ isActive }) => isActive ? navLinkActiveStyle : {}} to='projects'>Selected Works</NavLink>
+            {/* <NavLink className="header-menu-NavLink" onClick={() => props.setMenuOpen(!props.menuOpen)} style={({isActive})=>isActive ? navLinkActiveStyle : {}} to='contact'>Contact</NavLink> */}
+            {/* <NavLink className="header-menu-NavLink" onClick={() => props.setMenuOpen(!props.menuOpen)} style={({isActive})=>isActive ? navLinkActiveStyle : {}} to='login'>Login</NavLink> */}
+            {auth.auth ? <NavLink to="inbox" className="header-menu-NavLink" onClick={() => props.setMenuOpen(!props.menuOpen)} style={({ isActive }) => isActive ? navLinkActiveStyle : {}}>Inbox</NavLink> : <NavLink className="header-menu-NavLink"  onClick={() => props.setMenuOpen(!props.menuOpen)} to='contact' style={({ isActive }) => isActive ? navLinkActiveStyle : {}}>Contact</NavLink>}
+            {auth.auth ? <NavLink className="header-menu-NavLink"  onClick={() => props.setMenuOpen(!props.menuOpen)} to="/configuration" style={({ isActive }) => isActive ? navLinkActiveStyle : {}}>Configuration</NavLink> : <></>}
+            {auth.auth ? <a className="header-menu-NavLink" onClick={logoutHandler}>Logout</a> : <NavLink className="header-menu-NavLink"  onClick={() => props.setMenuOpen(!props.menuOpen)} to="/login" style={({ isActive }) => isActive ? navLinkActiveStyle : {}}>Login</NavLink>}
         </div>
     );
 }
