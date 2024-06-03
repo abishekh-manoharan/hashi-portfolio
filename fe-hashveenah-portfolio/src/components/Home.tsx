647fe87a16ed977f6074a74d7f6d5508e20d1eeb
abi-manoharan97 <abishekh.manoharan@gmail.com>
2024-06-03 12:47:19 -0400

    incorporate video into hp

--- fe-hashveenah-portfolio/src/components/Home.tsx
+++ fe-hashveenah-portfolio/src/components/Home.tsx
@@ -33,6 +33,13 @@ function Home(props: aboutProps) {
             </div>
             <Link className="home-art-link" to="/projects">View Selected Works</Link>
             <hr id="home-hr" />
+            <div className="video-container about-me-container">
+                {/* <hr className="about-me-hr-top" /> */}
+                <div className="home-info-title" style={{"textAlign":"center"}}> An Interview with Hashveenah Manoharan </div>
+                <div className="home-info-content" style={{"textAlign":"center"}}>Directed by Matthew Viveen </div>
+                <iframe className="video-container-video" src="https://www.youtube.com/embed/UtuTZn0gPuI?si=_K6v2sk3l7LqxBZW" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                {/* <hr className="about-me-hr-bottom" /> */}
+            </div>
         </div>
     );
 }
