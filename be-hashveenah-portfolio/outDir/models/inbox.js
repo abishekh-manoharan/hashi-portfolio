27c9c83b19c9efaee2ce895d4bdfb5d6cefd677b
abi-manoharan97 <abishekh.manoharan@gmail.com>
2024-06-04 12:56:01 -0400

    integrated front end build to be

--- be-hashveenah-portfolio/outDir/models/inbox.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-const inboxSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    message: String
-});
-export default mongoose.model('Message', inboxSchema);
